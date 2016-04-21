b77646ffac77ed0c9bd2e08739a76a4cdcccf2ae
gabrielleong <gabrielleong97@gmail.com>
2016-04-21 16:30:35 +0800

    adds join field, socket events and styling

--- app/components/chat-list-2.js
+++ app/components/chat-list-2.js
@@ -1,4 +1,40 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default Ember.Component.extend({
-});
+  init: function () {
+    this._super()
+    var socket = io.connect('http://ff-chat-app.herokuapp.com/')
+
+    socket.on('connect', function () {
+      $('#connectedServer').removeClass('red').addClass('blue black-text').text('Connected to server')
+    })
+    socket.on('disconnect', function () {
+      $('#connectedServer').removeClass('blue').addClass('red black-text').text('Not connected to server')
+      $('.progress').show()
+    })
+    socket.on('chat', function (data) {
+      $('.messages').prepend($('<li>').text(data.user.name + ': ' + data.message).addClass('li receivedMsg'))
+    })
+    socket.on('joined', function (user) {
+      $('.messages').prepend($('<li class="joinMsg">').text(user.name + ' joined the chat.'))
+    })
+    socket.on('left', function (user) {
+      console.log(user.name + ' left the chat.')
+      $('.messages').prepend($('<li class="joinMsg">').text(user.name + ' left the chat.'))
+    })
+    socket.on('online', function (connections) {
+      var names = ''
+      console.log('Connections: ', connections)
+      for (var i = 0; i < connections.length; ++i) {
+        if (connections[i].user) {
+          if (i > 0) {
+            if (i == connections.length - 1) names += ' and '
+            else names += ', '
+          }
+          names += connections[i].user.name
+        }
+      }
+      $('#connected').text(names).addClass('online')
+    })
+  }
+})
--- app/components/send-msg.js
+++ app/components/send-msg.js
@@ -1,16 +1,28 @@
-import Ember from 'ember';
-
+import Ember from 'ember'
+var check = 0
 export default Ember.Component.extend({
-   actions: {
-   sendMsg() {
-     var inputVal = $('input').val()
-     console.log(inputVal)
-     var user = {
-      name: 'Gab'
-     }
-     socket.emit("join", user);
-     socket.emit("chat", inputVal);
-     $('.messages').append($('<li>').text(inputVal));
-   }
- }
-});
+  actions: {
+    joinChat() {
+      var socket = io.connect('http://ff-chat-app.herokuapp.com/')
+
+      var user = {
+        name: 'anon'
+      }
+      user.name = $('#n').val()
+      console.log(user.name + 'joined!')
+      socket.emit('join', user)
+      $('.messages').append($('<li class="joinMsg">').text(user.name + ' joined!').addClass('joinMsg'))
+      $('#msgForm').show()
+      $('#joinForm').hide()
+    },
+    sendMsg() {
+      var socket = io.connect('http://ff-chat-app.herokuapp.com/')
+
+      var inputVal = $('#m').val()
+      console.log(inputVal)
+      socket.emit('chat', inputVal)
+      $('.messages').append($('<li>').text($('#n').val() + ' says: ' + inputVal).addClass('ownMsg'))
+      inputVal = $('#m').val('')
+    }
+  }
+})
